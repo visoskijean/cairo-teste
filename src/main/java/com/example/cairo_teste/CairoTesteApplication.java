8774b8278c34bfde6bd7c748ce531f8ef142fee4
jean visoski <ctl_jvisoski@uolinc.com>
2024-06-27 15:01:20 -0300

    test

---
+++ src/main/java/com/example/cairo_teste/CairoTesteApplication.java
@@ -0,0 +1,13 @@
+package com.example.cairo_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CairoTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CairoTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cairo_teste/CairoTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cairo_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CairoTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
